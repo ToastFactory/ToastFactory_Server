afb8361993d7a7205b07e4b2f613c474527a4fa2
minaver <psmjoshua@gmail.com>
2022-11-25 12:53:10 +0900

    init: Tykoon Game Server

---
+++ src/main/java/com/konkuk/toastfactory/ToastFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.konkuk.toastfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToastFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToastFactoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/konkuk/toastfactory/ToastFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konkuk.toastfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToastFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
